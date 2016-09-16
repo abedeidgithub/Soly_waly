88a0cf3b25fac698e72d27f8076204e83cc021db
abedeidgithub <abedeidgithub@gmail.com>
2016-09-16 14:44:37 +0200

    Final Project Soly

---
+++ app/src/main/java/com/example/abed/soly_waly_2/InterFace_File/Communicator.java
@@ -0,0 +1,10 @@
+package com.example.abed.soly_waly_2.InterFace_File;
+
+import com.example.abed.soly_waly_2.Model.Movie_Model_Class;
+
+/**
+ * Created by Soliman Waly on 16/09/2016.
+ */
+public interface Communicator {
+    public void sendDataOfMovies(Movie_Model_Class movie);
+}
